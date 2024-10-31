57ade753b7e13e9f2027168e88966ac7e0b14430
Bianka Ibrahim <biankaibrahim55@gmail.com>
2024-10-31 10:36:25 -0300

    alteração na configuração app.py

--- arquitetura/microsservicos/mult/app.py
+++ arquitetura/microsservicos/mult/app.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-app = FastAPI()
+app = FastAPI(__name__)
 
 @app.get("/mult")
 def mult(op1: float, op2: float):
-    return  op1 * op2
+    res = {}
+
+    if op1 is None or op2 is None:
+        raise HTTPException(status_code=400, detail="op1 ou op2 não informado")
     
+    res['resultado'] = op1 * op2
+    return res
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True)
