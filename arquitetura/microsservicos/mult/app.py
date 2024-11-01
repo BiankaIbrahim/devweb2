89f791b0599c552d3f83c3078cd26329afb90999
Bianka Ibrahim <biankaibrahim55@gmail.com>
2024-11-01 08:18:38 -0300

    Update app.py

--- arquitetura/microsservicos/mult/app.py
+++ arquitetura/microsservicos/mult/app.py
@@ -1,16 +1,19 @@
-from fastapi import FastAPI, HTTPException
+from flask import Flask
+from flask import request
+app = Flask(__name__)
 
-app = FastAPI(__name__)
-
-@app.get("/mult")
-def mult(op1: float, op2: float):
+@app.route("/mult", methods=["GET"])
+def mult():
     res = {}
+    op1 = request.args.get('op1')
+    op2 = request.args.get('op2')
 
-    if op1 is None or op2 is None:
-        raise HTTPException(status_code=400, detail="op1 ou op2 não informado")
-    
-    res['resultado'] = op1 * op2
-    return res
+    if not op1 or not op2:
+        res['resultado'] = "op1 ou op2 não informado"
+        return res, 400
+
+    res['resultado'] = float(op1) * float(op2)
+    return res, 200
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
